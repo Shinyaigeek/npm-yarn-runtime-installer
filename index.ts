71ff6ee26a6435b0ea0108b45d9a0fc6f9011f50
svpcadmin <PhilisPaxil@gmail.com>
2020-08-21 22:33:12 +0900

    initial commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+import { spawn } from "child_process";
+
+const install: (
+  modules: string[],
+  saveDevs: boolean,
+  manager?: "npm" | "yarn",
+  errHandle?: (err: string) => void
+) => void = (modules, saveDevs, manager, errHandle) => {
+  const isYarn = manager === "yarn";
+  const dev = isYarn ? "-D" : "--dev";
+  const cmd = isYarn ? "add" : "install";
+  const exe = [cmd, dev, ...modules];
+  const proc = spawn(isYarn ? "yarn" : "npm", exe);
+
+  let err = "";
+
+  if (proc.stderr) {
+    proc.stderr.on("data", (data) => {
+      err += data.toString();
+      errHandle && errHandle(err);
+    });
+  }
+
+  return proc;
+};
+
+const uninstall: (
+  modules: string[],
+  saveDevs: boolean,
+  manager?: "npm" | "yarn",
+  errHandle?: (err: string) => void
+) => void = (modules, saveDevs, manager, errHandle) => {
+  const isYarn = manager === "yarn";
+  const cmd = isYarn ? "yarn remove" : "npm uninstall";
+  const exe = `${cmd} ${modules.map((md) => md + " ")}`;
+  const proc = spawn(exe);
+
+  let err = "";
+
+  if (proc.stderr) {
+    proc.stderr.on("data", (data) => {
+      err += data.toString();
+      errHandle && errHandle(err);
+    });
+  }
+
+  return proc;
+};
+
+export { install, uninstall };
